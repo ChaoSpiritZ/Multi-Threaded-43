3af3978145eba954e9421b5bfae98e0bb7b78180
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2020-02-05 15:25:10 +0200

    Add files via upload

---
+++ Multi Threaded 43/Multi Threaded 43/AsyncPrinter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Multi_Threaded_43
+{
+    class AsyncPrinter
+    {
+        public List<string> _queue = new List<string>();
+        private object key = new object();
+        public bool sayThis = false; //just testing background threads
+
+        public void AddMessage(string message)
+        {
+            lock (key)
+            {
+                _queue.Add(message);
+                Monitor.Pulse(key);
+            }
+        }
+
+        public void CheckPrintMessage()
+        {
+            while (true)
+            {
+                lock (key)
+                {
+                    while (_queue.Count == 0)
+                    {
+                        Monitor.Wait(key);
+                    }
+                    Console.WriteLine(_queue[0]);
+                    _queue.RemoveAt(0);
+                }
+                if (sayThis) // testing background threads
+                {
+                    Console.WriteLine("oh hi there!");
+                }
+            }
+        }
+    }
+}
---
+++ Multi Threaded 43/Multi Threaded 43/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multi_Threaded_43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncPrinter AP = new AsyncPrinter();
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(AP.CheckPrintMessage);
+                threads[i].IsBackground = true;
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                AP.AddMessage($"message {i + 1}");
+                Thread.Sleep(100);
+            }
+            AP.sayThis = true; //testing background threads
+        }
+    }
+}
